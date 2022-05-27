d8ec426b1a293b7c18417ca53d2a4b0321f8cd5e
Milos Mladenovic <imilos994@gmail.com>
2022-05-27 08:26:49 +0200

    submit button

--- src/js/searchView.js
+++ src/js/searchView.js
@@ -5,11 +5,12 @@ getQuery(){
     const query = this._parentEl.querySelector('.search__field').value;
     // const src = this._parentEl.querySelector('.search__btn');
     console.log(query.length);
-    if(query.length<2){
+    if(query.length<2 || query.length===0 || query === ''){
         // alert('too short');
-        document.querySelector('.search__btn').setAttribute('disabled', 'disabled')
-    }else if(query.length > 3){
-        document.querySelector('.search__btn').removeAttribute('disabled')
+        document.querySelector('.search__btn').disabled = true;
+    }else{
+        document.querySelector('.search__btn').removeAttribute('disabled');
+        
     }
     this._clearInput();
     return query;
